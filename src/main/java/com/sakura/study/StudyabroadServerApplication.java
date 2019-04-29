199b344a6c7af8c43edc3138f0b36251f3062480
zhang <919179517@qq.com>
2019-04-29 21:12:32 +0800

    'init'

---
+++ src/main/java/com/sakura/study/StudyabroadServerApplication.java
@@ -0,0 +1,13 @@
+package com.sakura.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyabroadServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyabroadServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sakura/study/dao/package-info.java
@@ -0,0 +1,1 @@
+package com.sakura.study.dao;
