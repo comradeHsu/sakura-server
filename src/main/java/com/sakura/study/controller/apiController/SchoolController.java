ddb1e9f61b3e3b510410a616eb610d81629e6ddb
knockinHeavenDoor <61239289@qq.com>
2019-05-10 14:28:00 +0800

    first commit

--- src/main/java/com/sakura/study/controller/apiController/SchoolController.java
+++ src/main/java/com/sakura/study/controller/apiController/SchoolController.java
@@ -1,10 +1,12 @@
 package com.sakura.study.controller.apiController;
 
 import com.sakura.study.dto.UniversityPageRequest;
+import com.sakura.study.model.University;
 import com.sakura.study.service.UniversityService;
 import com.sakura.study.utils.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +29,16 @@ public class SchoolController {
         if(StringUtils.isEmpty(request.getName()))
             request.setName(null);
         return universityService.search(request);
+    }
+
+    /**
+     * 获取学校信息详情
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    public ResponseResult getDetail(@PathVariable("id") Integer id){
+        University university = universityService.getUniversityById(id);
+        return ResponseResult.success(university);
     }
 }
